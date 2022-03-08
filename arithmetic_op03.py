c50e4ceda5553c8f65582a6c10c94eb56dbe56b1
Umidbek <85737786+Mr-Umidjon@users.noreply.github.com>
2022-03-08 13:03:50 +0500

    Create arithmetic_op03.py

---
+++ arithmetic_op03.py
@@ -0,0 +1,7 @@
+# number nomli ikki honali butun sonli o’zgaruvchi yarating.
+# answer nomli o’zgaruvchiga number ning teskarisini taminlang.
+# Natijani chop eting.
+
+number = 52
+answer = number % 10 * 10 + number // 10
+print(answer)

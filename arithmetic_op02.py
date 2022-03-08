2901bf31567b6ecdbed2d4d404b90f18c667147c
Umidbek <85737786+Mr-Umidjon@users.noreply.github.com>
2022-03-08 13:01:46 +0500

    Create arithmetic_op02.py

---
+++ arithmetic_op02.py
@@ -0,0 +1,8 @@
+# number nomli o’zgaruvchi yarating va 43 ni taminlang.
+# answer nomli o’zgaruvchi yarating va number ning raqamlari yig’indisini
+# taminlang.
+# Natijani chop eting.
+
+number = 43
+answer = number // 10 + number % 10
+print(answer)

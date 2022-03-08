5c92643d4b93fa7a451d394a90a5007d39eea24f
Umidbek <85737786+Mr-Umidjon@users.noreply.github.com>
2022-03-08 13:11:08 +0500

    Create arithmetic_op06.py

---
+++ arithmetic_op06.py
@@ -0,0 +1,11 @@
+# number nomli o’zgaruvchi yarating va unga butun son taminlang.
+# number ni 2 ga bo’ling va number ga taminlang.
+# 2 bo’lgandagi kasr qismini toping va answer o’zgaruvchiga taminlang.
+# answer ni chop eting.
+# number ni chop eting.
+
+number = 123
+number /= 2
+answer = number % 1
+print(number)
+print(answer)

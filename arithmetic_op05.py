6659af1c8b89ba9cf2f64498f4f82099d4c80039
Umidbek <85737786+Mr-Umidjon@users.noreply.github.com>
2022-03-08 13:08:16 +0500

    Create arithmetic_op05.py

---
+++ arithmetic_op05.py
@@ -0,0 +1,8 @@
+# number nomli o’zgaruvchi yarating va unga butun son taminlang.
+# answer nomli o’zgaruvchi yarating.
+# number o’zgaruvchini 3 ga bo’lgandagi qoldiqni answer ga taminlang.
+# Natijani chop eting.
+
+number = 391
+answer = number % 3
+print(answer)
